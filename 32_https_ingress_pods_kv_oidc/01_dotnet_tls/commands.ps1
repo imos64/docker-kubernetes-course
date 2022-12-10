4f0a888af578e6539c51b59f816573107612fa80
Houssem Dellai <hodellai@microsoft.com>
2022-12-10 09:04:17 +0100

    working..

--- 32_https_ingress_pods_kv_oidc/01_dotnet_tls/commands.ps1
+++ 32_https_ingress_pods_kv_oidc/01_dotnet_tls/commands.ps1
@@ -1,8 +1,7 @@
-# Setting TLS certificate in Kubernetes deployment
+# Lab: setting TLS certificate in Kubernetes deployment
 
-## 1. Create an AKS cluster
+# 1. Create an AKS cluster
 
-```powershell
 $AKS_RG="rg-aks-demo-tls"
 $AKS_NAME="aks-cluster"
 
@@ -13,22 +12,15 @@ az aks create -g $AKS_RG -n $AKS_NAME `
               --enable-managed-identity `
               --node-count 2 `
               --network-plugin azure
-```
 
-## Connect to the cluster
+# Connect to the cluster
 
-```powershell
 az aks get-credentials -n $AKS_NAME -g $AKS_RG --overwrite-existing
 
 kubectl get nodes
-# NAME                                STATUS   ROLES   AGE     VERSION
-# aks-nodepool1-17393022-vmss000000   Ready    agent   3m13s   v1.25.2
-# aks-nodepool1-17393022-vmss000001   Ready    agent   3m24s   v1.25.2
-```
 
-## 2. Create TLS certificate for the Deployment
+# 2. Create TLS certificate for the Deployment
 
-```powershell
 $APP_CERT_NAME="app-tls-cert"
 
 $SERVICE_NAME="app-svc"
@@ -38,45 +30,23 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 `
     -keyout "${APP_CERT_NAME}.key" `
     -subj "/CN=$SERVICE_NAME.$NAMESPACE_APP.svc.cluster.local/O=aks-ingress-tls" `
     -addext "subjectAltName=DNS:$SERVICE_NAME.$NAMESPACE_APP.svc.cluster.local"
-# Generating a RSA private key
-# ........+++++
-# ....................................+++++
-# writing new private key to 'app-tls-cert.key'
 
 openssl pkcs12 -export -in "$APP_CERT_NAME.crt" -inkey "$APP_CERT_NAME.key" -out "$APP_CERT_NAME.pfx"
-# Enter Export Password:
-# Verifying - Enter Export Password:
-```
 
-## 3. Save certificate into a kubernetes secret object
+# 3. Save certificate into a kubernetes secret object
 
-```powershell
 $NAMESPACE_APP="dotnet-app"
 
 kubectl create namespace $NAMESPACE_APP
-# namespace/dotnet-app created
 
 $APP_SECRET_TLS="app-tls-cert-secret"
 
 kubectl create secret generic $APP_SECRET_TLS --from-file="$APP_CERT_NAME.pfx" --namespace $NAMESPACE_APP
-# secret/app-tls-cert-secret created
 
 kubectl describe secret $APP_SECRET_TLS --namespace $NAMESPACE_APP
-# Name:         app-tls-cert-secret
-# Namespace:    dotnet-app
-# Labels:       <none>
-# Annotations:  <none>
 
-# Type:  Opaque
+# 4. Deploy sample application that uses the TLS secret to configure HTTPS
 
-# Data
-# ====
-# app-tls-cert.pfx:  2509 bytes
-```
-
-## 4. Deploy sample application that uses the TLS secret to configure HTTPS
-
-```powershell
 @"
 apiVersion: v1
 kind: Service
@@ -135,40 +105,11 @@ spec:
 "@  > app-deploy-svc.yaml
 
 kubectl apply -f app-deploy-svc.yaml -n $NAMESPACE_APP
-```
 
-Check app deployed
-
-```powershell
 kubectl get pods,svc -n $NAMESPACE_APP
-# NAME                            READY   STATUS    RESTARTS   AGE
-# pod/demo-app-5bcc468b9f-ldrhk   1/1     Running   0          17m
-# pod/demo-app-5bcc468b9f-mgr6x   1/1     Running   0          17m
-# pod/demo-app-5bcc468b9f-qv85h   1/1     Running   0          17m
 
-# NAME              TYPE        CLUSTER-IP   EXTERNAL-IP   PORT(S)   AGE
-# service/app-svc   ClusterIP   10.0.96.70   <none>        443/TCP   17m
-```
+# 5. Verify TLS certificate is working
 
-## 5. Verify TLS certificate is working
-
-```powershell
 kubectl run nginx --image=nginx
 
 kubectl exec -it nginx -- curl -v -k https://app-svc.dotnet-app.svc.cluster.local
-# *   Trying 10.0.96.70:443...
-# * Connected to app-svc.dotnet-app.svc.cluster.local (10.0.96.70) port 443 (#0)
-# * ALPN, offering h2
-# * ALPN, offering http/1.1
-# * successfully set certificate verify locations:
-# *  CAfile: /etc/ssl/certs/ca-certificates.crt
-# *  CApath: /etc/ssl/certs
-# * TLSv1.3 (OUT), TLS handshake, Client hello (1):
-# * SSL connection using TLSv1.3 / TLS_AES_256_GCM_SHA384
-# * Server certificate:
-# *  subject: CN=app-svc..svc.cluster.local; O=aks-ingress-tls
-# *  start date: Dec 10 07:42:41 2022 GMT
-# *  expire date: Dec 10 07:42:41 2023 GMT
-# *  issuer: CN=app-svc..svc.cluster.local; O=aks-ingress-tls
-# *  SSL certificate verify result: self signed certificate (18), continuing anyway.
-```
